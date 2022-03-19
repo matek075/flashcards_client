3f5eb5d02d8c488aacf4ca047d9f4bc67088e374
Reckit075 <mateusz.majka.zsl@gmail.com>
2022-03-19 17:54:10 +0100

    fix: url api to register user

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,7 @@
 export const url_userLogin ='https://flash-cards-server2.herokuapp.com/users/login'
 export const url_userGetAll ='https://flash-cards-server2.herokuapp.com/users/all'
 export const url_userGetOne ='https://flash-cards-server2.herokuapp.com/users/one'
-export const url_userRegister ='https://flash-cards-server2.herokuapp.com//users/register'
+export const url_userRegister ='https://flash-cards-server2.herokuapp.com/users/register'
 export const url_userCode ='https://flash-cards-server2.herokuapp.com/users/code'
 export const url_userDeleteDatabase ='https://flash-cards-server2.herokuapp.com/users/deleteDataBase'
 
